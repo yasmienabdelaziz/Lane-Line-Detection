a7265ed613e3f70dae9fcf2f399b103fa6f4f0b2
BaSsioonii <63972053+BaSsioonii@users.noreply.github.com>
2022-04-20 14:24:28 +0200

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -79,3 +79,39 @@ def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
     combined_binary = np.zeros_like(sxbinary)
     combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
     return combined_binary
+
+def perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
+                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv2.warpPerspective(img, M, dst_size)
+    return warped
+
+def inv_perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0,0), (1, 0), (0,1), (1,1)]),
+                     dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv2.warpPerspective(img, M, dst_size)
+    return warped
+
+def get_hist(img):
+    hist = np.sum(img[img.shape[0]//2:,:], axis=0)
+    return hist
