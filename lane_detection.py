40977ae8198ceba301cc8d2ecab34f130f4ac422
Yasmien Abdelaziz <52866187+yasmienabdelaziz@users.noreply.github.com>
2022-04-20 22:10:26 +0200

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -1,3 +1,4 @@
+from moviepy.editor import VideoFileClip
 import numpy as np
 import pandas as pd
 import cv2
@@ -7,10 +8,11 @@ import matplotlib.pyplot as plt
 import pickle
 import sys
 
+
 def undistort_img():
     # Prepare object points 0,0,0 ... 8,5,0
-    obj_pts = np.zeros((6*9,3), np.float32)
-    obj_pts[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+    obj_pts = np.zeros((6*9, 3), np.float32)
+    obj_pts[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 
     # Stores all object points & img points from all images
     objpoints = []
@@ -23,7 +25,7 @@ def undistort_img():
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
 
         if ret == True:
             objpoints.append(obj_pts)
@@ -32,14 +34,16 @@ def undistort_img():
     img_size = (img.shape[1], img.shape[0])
 
     # Calibrate camera
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None,None)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, img_size, None, None)
 
     dst = cv2.undistort(img, mtx, dist, None, mtx)
     # Save camera calibration for later use
     dist_pickle = {}
     dist_pickle['mtx'] = mtx
     dist_pickle['dist'] = dist
-    pickle.dump( dist_pickle, open('camera_cal/cal_pickle.p', 'wb') )
+    pickle.dump(dist_pickle, open('camera_cal/cal_pickle.p', 'wb'))
+
 
 def undistort(img, cal_dir='camera_cal/cal_pickle.p'):
     #cv2.imwrite('camera_cal/test_cal.jpg', dst)
@@ -48,45 +52,53 @@ def undistort(img, cal_dir='camera_cal/cal_pickle.p'):
     mtx = file['mtx']
     dist = file['dist']
     dst = cv2.undistort(img, mtx, dist, None, mtx)
-    
+
     return dst
+
+
 undistort_img()
+
 
 def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
     img = undistort(img)
     img = np.copy(img)
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
-    l_channel = hls[:,:,1]
-    s_channel = hls[:,:,2]
-    h_channel = hls[:,:,0]
+    l_channel = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+    h_channel = hls[:, :, 0]
     # Sobel x
-    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 1) # Take the derivative in x
-    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 1)  # Take the derivative in x
+    # Absolute x derivative to accentuate lines away from horizontal
+    abs_sobelx = np.absolute(sobelx)
     scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
-    
+
     # Threshold x gradient
     sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
-    
+    sxbinary[(scaled_sobel >= sx_thresh[0]) &
+             (scaled_sobel <= sx_thresh[1])] = 1
+
     # Threshold color channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
-    
-    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
-    
+
+    color_binary = np.dstack(
+        (np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+
     combined_binary = np.zeros_like(sxbinary)
     combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
     return combined_binary
 
-def perspective_warp(img, 
-                     dst_size=(1280,720),
-                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
-                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
-    img_size = np.float32([(img.shape[1],img.shape[0])])
-    src = src* img_size
+
+def perspective_warp(img,
+                     dst_size=(1280, 720),
+                     src=np.float32(
+                         [(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)]),
+                     dst=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)])):
+    img_size = np.float32([(img.shape[1], img.shape[0])])
+    src = src * img_size
     # For destination points, I'm arbitrarily choosing some points to be
-    # a nice fit for displaying our warped result 
+    # a nice fit for displaying our warped result
     # again, not exact, but close enough for our purposes
     dst = dst * np.float32(dst_size)
     # Given src and dst points, calculate the perspective transform matrix
@@ -95,14 +107,15 @@ def perspective_warp(img,
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped
 
-def inv_perspective_warp(img, 
-                     dst_size=(1280,720),
-                     src=np.float32([(0,0), (1, 0), (0,1), (1,1)]),
-                     dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
-    img_size = np.float32([(img.shape[1],img.shape[0])])
-    src = src* img_size
+
+def inv_perspective_warp(img,
+                         dst_size=(1280, 720),
+                         src=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)]),
+                         dst=np.float32([(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)])):
+    img_size = np.float32([(img.shape[1], img.shape[0])])
+    src = src * img_size
     # For destination points, I'm arbitrarily choosing some points to be
-    # a nice fit for displaying our warped result 
+    # a nice fit for displaying our warped result
     # again, not exact, but close enough for our purposes
     dst = dst * np.float32(dst_size)
     # Given src and dst points, calculate the perspective transform matrix
@@ -110,10 +123,12 @@ def inv_perspective_warp(img,
     # Warp the image using OpenCV warpPerspective()
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped
+
 
 def get_hist(img):
-    hist = np.sum(img[img.shape[0]//2:,:], axis=0)
+    hist = np.sum(img[img.shape[0]//2:, :], axis=0)
     return hist
+
 
 def vid_pipeline(img):
     global running_avg
@@ -121,16 +136,20 @@ def vid_pipeline(img):
     img_ = pipeline(img)
     img_ = perspective_warp(img_)
     out_img, curves, lanes, ploty = sliding_window(img_, draw_windows=True)
-    curverad =get_curve(img, curves[0], curves[1])
+    curverad = get_curve(img, curves[0], curves[1])
     lane_curve = np.mean([curverad[0], curverad[1]])
     img = draw_lanes(img, curves[0], curves[1])
-    
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     fontColor = (0, 0, 0)
-    fontSize=0.5
-    cv2.putText(img, 'Lane Curvature: {:.0f} m'.format(lane_curve), (570, 620), font, fontSize, fontColor, 2)
-    cv2.putText(img, 'Vehicle offset: {:.4f} m'.format(curverad[2]), (570, 650), font, fontSize, fontColor, 2)
+    fontSize = 0.5
+    cv2.putText(img, 'Lane Curvature: {:.0f} m'.format(
+        lane_curve), (570, 620), font, fontSize, fontColor, 2)
+    cv2.putText(img, 'Vehicle offset: {:.4f} m'.format(
+        curverad[2]), (570, 650), font, fontSize, fontColor, 2)
     return img
+
+
 left_a, left_b, left_c = [], [], []
 right_a, right_b, right_c = [], [], []
 
@@ -231,6 +250,7 @@ def sliding_window(img, nwindows=9, margin=150, minpix=1, draw_windows=True):
 
     return out_img, (left_fitx, right_fitx), (left_fit_, right_fit_), ploty
 
+
 def get_curve(img, leftx, rightx):
     ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
     y_eval = np.max(ploty)
@@ -272,45 +292,51 @@ def draw_lanes(img, left_fit, right_fit):
 
 
 videopath = sys.argv[1]
-#output_location = sys.argv[2]
-mode = int(sys.argv[3])
+output_location = sys.argv[2]
+mode = int(sys.argv[4])
 
-cap=cv2.VideoCapture(videopath)
-while(cap.isOpened()):
-    _,pure_frame=cap.read()
-    frame1=pure_frame
-    frame2=pure_frame
-    out_frame=vid_pipeline(pure_frame)
-    if(mode==0):
-        cv2.imshow("output",out_frame)
-        if cv2.waitKey(1)&0xFF==ord('q'):
-            break
-    elif(mode==1):
+if (mode == 0):
+    myclip = VideoFileClip(videopath)  # .subclip(40,43)
+    output_vid = output_location
+    clip = myclip.fl_image(vid_pipeline)
+    clip.write_videofile(output_vid, audio=False)
+elif(mode == 1):
+    cap = cv2.VideoCapture(videopath)
+    while(cap.isOpened()):
+        _, pure_frame = cap.read()
+        frame1 = pure_frame
+        frame2 = pure_frame
+        out_frame = vid_pipeline(pure_frame)
         dst = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
-    #converting from RGB to thresholded HLS
+        # converting from RGB to thresholded HLS
         HLS = pipeline(dst)
         BirdEyeHLS = perspective_warp(HLS)
-        BirdEye=perspective_warp(pure_frame)
-        windows, curves, lanes, ploty = sliding_window(BirdEyeHLS, draw_windows=True)
+        BirdEye = perspective_warp(pure_frame)
+        windows, curves, lanes, ploty = sliding_window(
+            BirdEyeHLS, draw_windows=True)
 
-    
-    
         BirdEyeHLS = cv2.cvtColor(BirdEyeHLS, cv2.COLOR_GRAY2RGB)
         HLS = cv2.cvtColor(HLS, cv2.COLOR_GRAY2RGB)
-        BirdEyeHLS=cv2.resize(BirdEyeHLS,(640,360),interpolation=cv2.INTER_AREA)
-        HLS=cv2.resize(HLS,(640,360),interpolation=cv2.INTER_AREA)
-        BirdEye=cv2.resize(BirdEye,(640,360),interpolation=cv2.INTER_AREA)
-        windows=cv2.resize(windows,(640,360),interpolation=cv2.INTER_AREA)
-        out_frame=cv2.resize(out_frame,(640,360),interpolation=cv2.INTER_AREA)
-        pure_frame=cv2.resize(pure_frame,(640,360),interpolation=cv2.INTER_AREA)
-        BirdEyeHLS = cv2.normalize(BirdEyeHLS, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        HLS= cv2.normalize(HLS, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        Hori = np.hstack((pure_frame, HLS,BirdEye))
-        Hori2 = np.hstack((BirdEyeHLS,windows, out_frame))
-        ver=np.vstack((Hori,Hori2))
-        cv2.imshow("output",ver)
-        if cv2.waitKey(1)&0xFF==ord('q'):
+        BirdEyeHLS = cv2.resize(
+            BirdEyeHLS, (640, 360), interpolation=cv2.INTER_AREA)
+        HLS = cv2.resize(HLS, (640, 360), interpolation=cv2.INTER_AREA)
+        BirdEye = cv2.resize(BirdEye, (640, 360),
+                             interpolation=cv2.INTER_AREA)
+        windows = cv2.resize(windows, (640, 360),
+                             interpolation=cv2.INTER_AREA)
+        out_frame = cv2.resize(out_frame, (640, 360),
+                               interpolation=cv2.INTER_AREA)
+        pure_frame = cv2.resize(
+            pure_frame, (640, 360), interpolation=cv2.INTER_AREA)
+        BirdEyeHLS = cv2.normalize(
+            BirdEyeHLS, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        HLS = cv2.normalize(HLS, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        Hori = np.hstack((pure_frame, HLS, BirdEye))
+        Hori2 = np.hstack((BirdEyeHLS, windows, out_frame))
+        ver = np.vstack((Hori, Hori2))
+        cv2.imshow("output", ver)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-cap.release()
-cv2.destroyAllWindows()
-
+    cap.release()
+    cv2.destroyAllWindows()
+    
